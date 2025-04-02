3c86de8b100ca75bac68446de53aee1fb03e824a
XoDeR <xoder.vs@gmail.com>
2025-04-02 22:52:57 +0200

    model, mock, GetAllBooks endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -2,20 +2,33 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/XoDeR/simple-rest-api-go/pkg/handlers"
 	"github.com/gorilla/mux" // routing
 )
 
 func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/books", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode("BookSeller API")
 	})
 
+	router.HandleFunc("/books", handlers.GetAllBooks).Methods(http.MethodGet)
+
 	log.Println("API server is running!")
 	http.ListenAndServe(":4000", router)
+
+	return nil
 }
---
+++ pkg/mocks/book.go
@@ -0,0 +1,12 @@
+package mocks
+
+import "github.com/XoDeR/simple-rest-api-go/pkg/models"
+
+var Books = []models.Book{
+	{
+		Id:     1,
+		Title:  "Prejudice",
+		Author: "Maurice Druon",
+		Desc:   "A novel for young people",
+	},
+}
